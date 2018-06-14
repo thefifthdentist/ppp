c7364d4b2d3d3c4a32d758ac6677a593ddb7dc98
thefifthdentist <37271419+thefifthdentist@users.noreply.github.com>
2018-06-13 22:24:43 -0400

    pre-submission and unfortunately completion

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -5,3 +5,14 @@ var instance = M.Sidenav.init(elem, options);
 
 /*var elem = document.querySelector('.fixed-action-btn');
 var instance = M.FloatingActionButton.init(elem, options); */
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.materialboxed');
+  var instances = M.Materialbox.init(elems, options);
+});
+
+// Or with jQuery
+// 
+// $(document).ready(function(){
+//   $('.materialboxed').materialbox();
+// });
